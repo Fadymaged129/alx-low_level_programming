742065dcfd1007e408d9be44f8ce84f09a1e6b8d
fady <fadymaged129@gmail.com>
2023-04-20 16:44:14 -0700

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("\n");
 		return;
 	}
-	va_start(ap, n)
+	va_start(ap, n);
 	while (i--)
 		printf("%d%s", va_arg(ap, int), i ? (separator : "") : "\n");
 	va_end(ap);
